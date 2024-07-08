cb87baea92994b04f1f898efa6e3ecd92270f0b3
Harreson Lima <contatoharresonlima@gmail.com>
2024-07-08 20:48:39 -0300

    updated

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/prop-types */
 import styles from "./Block.module.css"
 
 function Block({card}) {
+
   return (
     <div className={`${styles.block} ${card && styles.card}`}>
     </div>
