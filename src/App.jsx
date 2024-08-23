520c6f7dd71a2411607b6844ea7ba357df974ff8
Harreson Lima <contatoharresonlima@gmail.com>
2024-08-22 22:22:41 -0300

    v1 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faWind, faDroplet, faGauge, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faWind, faDroplet, faGauge, faSun, faMoon, faCloudRain } from '@fortawesome/free-solid-svg-icons'
 
 import MainBox from './components/MainBox';
 
 function App() {
   
   const [data, setData] = useState({});
+  const [forecast, setForecast] = useState({});
   const [location, setLocation] = useState('Thredbo');
 
   const api_key = '4a84acaf481f1d9ea2f93508c9f77572';
@@ -24,8 +25,6 @@ function App() {
     setLocation(e.target.value);
   };
 
-  // api.openweathermap.org/data/2.5/forecast?lat=-24.7911&lon=-50.0119&appid=4a84acaf481f1d9ea2f93508c9f77572
-
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       search();
@@ -34,7 +33,7 @@ function App() {
 
   const search = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
-
+    
     fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -47,10 +46,26 @@ function App() {
       }
       return response.json();
     })
-    .then(data => setData(data))
+    .then(data => {
+      setData(data)
+      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=Metric&appid=${api_key}`;
+      fetch(forecastUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error in request');
+        }
+        return response.json();
+      })
+      .then(data => setForecast(data))
+      .catch(error => {
+        console.error('An error occurred:', error);
+    });
+    })
     .catch(error => {
       console.error('An error occurred:', error);
     });
+
+    console.log(forecast)
     setLocation('');
   };
 
@@ -106,7 +121,11 @@ function App() {
           <FontAwesomeIcon icon={faGauge}/>
           <span>{data.main && data.main.pressure}hPa</span>
         </div>
-        <div className='bento__box simple_box flex' title='Sunrise/Sunset'>
+        <div className='bento__box simple_box flex' title='Rain probability'>
+          <FontAwesomeIcon icon={faCloudRain}/>
+          <span>{forecast.list && forecast.list[0].pop}%</span>
+        </div>
+        <div className='bento__box simple_box column_span--2 flex' title='Sunrise/Sunset'>
           <div className='flex time'>
             <FontAwesomeIcon icon={faSun}/>
             <span>{data.sys && convertTime(data.sys.sunrise,data.timezone)}</span>
@@ -116,43 +135,63 @@ function App() {
             <span>{data.sys && convertTime(data.sys.sunset,data.timezone)}</span>
           </div>
         </div>
-        <div className='bento__box column_span--2'></div>
-        <div className='bento__box column_span--3'></div>
+        <div className='bento__box column_span--3 flex'>
+
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[1].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[1].main.temp)}°
+            </div>
+          </div>
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[9].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[9].main.temp)}°
+            </div>
+          </div>
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[17].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[17].main.temp)}°
+            </div>
+          </div>
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[25].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[25].main.temp)}°
+            </div>
+          </div>
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[33].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[33].main.temp)}°
+            </div>
+          </div>
+          <div className="day flex">
+            <span className="day__date">
+              Day {forecast.list && forecast.list[39].dt_txt.split(' ')[0].split('-')[2]}
+            </span>
+            <div className="day__temp">
+              {forecast.list && Math.floor(forecast.list[39].main.temp)}°
+            </div>
+          </div>
+
+          
+
+        </div>
       </div>
     </div>
   )
 }
 
 export default App
-
-
-/*
-  const [data, setData] = useState({});
-  const [location, setLocation] = useState("Castro");
-
-  const api_key = "4a84acaf481f1d9ea2f93508c9f77572";
-
-  useEffect(() => {
-    search();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const handleInputChange = (e) => {
-    setLocation(e.target.value);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      search();
-    }
-  };
-
-  const search = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
-    const response = await fetch(url);
-    const searchData = await response.json();
-    setData(searchData);
-    setLocation("");
-  };
-
-*/
